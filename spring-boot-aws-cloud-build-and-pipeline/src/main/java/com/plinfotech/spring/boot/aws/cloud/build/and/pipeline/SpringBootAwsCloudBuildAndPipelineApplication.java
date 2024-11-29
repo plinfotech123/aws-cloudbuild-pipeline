bb51817ec289f1c6e78b2c4232be855e604ea247
plinfotech123 <plinfotech123@gmail.com>
2024-11-29 08:22:55 -0600

    Added test api

--- spring-boot-aws-cloud-build-and-pipeline/src/main/java/com/plinfotech/spring/boot/aws/cloud/build/and/pipeline/SpringBootAwsCloudBuildAndPipelineApplication.java
+++ spring-boot-aws-cloud-build-and-pipeline/src/main/java/com/plinfotech/spring/boot/aws/cloud/build/and/pipeline/SpringBootAwsCloudBuildAndPipelineApplication.java
@@ -1,13 +1,24 @@
-package src.main.java.com.plinfotech.spring.boot.aws.cloud.build.and.pipeline;
+package com.plinfotech.spring.boot.aws.cloud.build.and.pipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/test")
 public class SpringBootAwsCloudBuildAndPipelineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAwsCloudBuildAndPipelineApplication.class, args);
+	}
+
+
+	@GetMapping
+	public String test(){
+	   return  "Aws cloud build and pipeline testing";
 	}
 
 }
