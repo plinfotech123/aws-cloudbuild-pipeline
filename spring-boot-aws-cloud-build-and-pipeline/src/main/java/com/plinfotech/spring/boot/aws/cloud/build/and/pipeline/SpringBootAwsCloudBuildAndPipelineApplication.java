cb8ccf5048a5b000d9d6bb9e5de5171f2740c737
plinfotech123 <plinfotech123@gmail.com>
2024-11-29 08:18:07 -0600

    Added course-service

---
+++ spring-boot-aws-cloud-build-and-pipeline/src/main/java/com/plinfotech/spring/boot/aws/cloud/build/and/pipeline/SpringBootAwsCloudBuildAndPipelineApplication.java
@@ -0,0 +1,13 @@
+package src.main.java.com.plinfotech.spring.boot.aws.cloud.build.and.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsCloudBuildAndPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsCloudBuildAndPipelineApplication.class, args);
+	}
+
+}
---
+++ spring-boot-aws-cloud-build-and-pipeline/src/test/java/com/plinfotech/spring/boot/aws/cloud/build/and/pipeline/SpringBootAwsCloudBuildAndPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plinfotech.spring.boot.aws.cloud.build.and.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsCloudBuildAndPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
